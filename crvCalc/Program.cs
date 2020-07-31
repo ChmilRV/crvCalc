e752ca029c90d72bfdf4fd8f5544c94d2949768e
chmilrv@gmail.com <chmilrv@gmail.com>
2020-07-31 16:03:12 +0300

    1603

--- crvCalc/Program.cs
+++ crvCalc/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using static System.Console;
+using System.Globalization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -90,9 +91,6 @@ namespace crvCalc
                     value = value.Insert(value.IndexOf(")(") + 1, "*");
                 }
 
-
-
-
                 while (value.Substring(value.IndexOf('(') - 1, 1).IndexOfAny(new char[] { '*', '/', '+', '-' }) == -1)
                 {
                     value = value.Insert(value.IndexOf('('), "*");
@@ -113,15 +111,14 @@ namespace crvCalc
 
         public static string FindBracket(string _expr)
         {
-            //поиск ()
             int closeBracket = _expr.IndexOf(')');
             int openBracket = (_expr.Substring(0, closeBracket)).LastIndexOf('(');
-            //вырезание подстроки
             string sub = _expr.Substring(openBracket + 1, closeBracket - openBracket - 1);
             return sub;
         }
         public static string BracketsToSimple(string sub)
         {
+            IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "." };
             char[] opers = { '*', '/', '+', '-' };
             while (sub.IndexOfAny(opers) != -1)
             {
@@ -147,8 +144,8 @@ namespace crvCalc
 
                             if (sub.IndexOf(numbers[i], sub.IndexOf(op)) == sub.IndexOf(op) + 1)
                             {
-                                double num1 = Convert.ToDouble(numbers[i - 1]);
-                                double num2 = Convert.ToDouble(numbers[i]);
+                                double num1 = Convert.ToDouble(numbers[i - 1], formatter);
+                                double num2 = Convert.ToDouble(numbers[i], formatter);
                                 Operations calc = new Operations(num1, num2);
                                 double result = 0;
                                 switch (op)
@@ -196,7 +193,7 @@ namespace crvCalc
         {
             Title="crvCalc v0.01";
 
-            string testString = "152 - 2(100+5) ( 12+8)+(34,8-7(72,5+5,98)(78-5)+5(6-5)/4)";  //=-44115,23 - ?
+            string testString = "152 - 2(100+5) ( 12+8)+(34.8-7(72.5+5.98)(78-5)+5(6-5)/4)";  //=-44115.23 - ?
 
             WriteLine(testString);
             ExpressionLogic exp1 = new ExpressionLogic(testString);
